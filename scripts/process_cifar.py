d76a7a779a7a8e446385a14025f2464d85fb9b21
Nicholas Vincent <nickmvincent@gmail.com>
2020-01-11 02:05:29 -0600

    WIP: helper to merge CIFAR results from different runs together A bit hacky still.

--- scripts/process_cifar.py
+++ scripts/process_cifar.py
@@ -1,11 +1,28 @@
 #%%
 import pandas as pd
-df = pd.read_csv('results/cifar_cdc_0.csv', index_col=0).reset_index()
+import glob
 
+folder = 'results/cifar10'
+dfs = []
+for path in glob.glob(folder + '/*'):
+    df = pd.read_csv(path, index_col=0).dropna(axis=1,how='all')
+    print(df.head())
+    grouped = df.groupby(['company', 'frac'])
+    for name, group in grouped:
+        c = group.copy()
+        test_acc_row = c.iloc[-1]
+        c = c[(c.epoch == 24) & (c['test acc'] == 0)]
+        c['test_acc'] = test_acc_row['test acc']
+        print(test_acc_row['test acc'])
+        print(c.head())
+        print('==')
+        dfs.append(c)
+
+merged = pd.concat(dfs, sort=True)
 
 # %%
-df[df.epoch == 24][
-    ['company', 'frac', 'seed', 'valid_acc']
-].to_csv('results/cifar_rows.csv')
+merged[
+    ['company', 'frac', 'seed', 'valid_acc', 'test_acc']
+].to_csv('results/cifar10_rows.csv')
 
 # %%
